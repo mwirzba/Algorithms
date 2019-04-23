da68925bb9468dc9f343e2e5dc376c7c74075076
Matetus <mateuszwir23@gmail.com>
2019-04-23 18:51:30 +0200

    AddingBtree

---
+++ BTree/BTree/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTree
+{
+    class Node
+    {
+        //   private List<int> keys = new List<int> {get;set;}
+        public  List<Node> childs { get; set; } = new List<Node>();
+        public bool IsLeaf { get; set; }
+        public List<int> keys { get; set; } = new List<int>();
+
+
+}
+}
