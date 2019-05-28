c5bd9f74372ac61ce30543d9ec68c208bb15f1a5
mwirzba <mateuszwir23@gmail.com>
2019-05-28 23:37:36 +0200

    Adding FindingPatternsAlgorithm

---
+++ PatternsInTextAlgorithms/PatternsInTextAlgorithms/Algorithms.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsInTextAlgorithms
+{
+    public class Algorithms
+    {
+        public void Naive(string pattern, string text)
+        {
+          
+            for (int i = 0; i < text.Length - pattern.Length; i++)
+            {
+                int j = 0;
+                for (j = 0; j < pattern.Length; j++)
+                    if (text[i + j] != pattern[j])
+                        break;
+                if (j == pattern.Length)
+                    Console.WriteLine("Znaleziono wzorzec na pozycji " + i);
+            }
+
+        }
+
+        public void Rabin_Karp(string pattern, string text,int d,int q)
+        {
+            int hash = 1;
+            for (int i = 0; i < pattern.Length-1; i++)
+                hash = (hash * d) % q;
+
+            int patternH = 0;
+            int currTextH = 0;
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                patternH = (d * patternH + pattern[i]) % q;
+                currTextH = (d * currTextH + text[i]) % q;
+            }
+
+            for (int i = 0; i < text.Length-pattern.Length; i++)
+            {
+                if (patternH == currTextH)
+                {
+                    int j = 0;
+                    for (j = 0; j < pattern.Length; j++)
+                    {
+                        if (text[i + j] != pattern[j])
+                            break;
+                    }
+
+                    if (j == pattern.Length)
+                        Console.WriteLine("Znaleziono wzorzec na pozycji " + i);
+                }
+
+                if (i < text.Length - pattern.Length)
+                {
+                    int newHash = (text[i] * hash) % q;
+                    if (currTextH < newHash)
+                    {
+                        currTextH = currTextH + q;
+                    }
+
+                    currTextH = (d * (currTextH - newHash) + text[i + pattern.Length]) % q;
+              
+                }
+            }
+        }
+
+
+        public void KMP(string text, string pattern)
+        {
+            int q = 0;
+            int[] prefixTable = Prefix_Function(pattern);
+            int patternIterator = 0; // pattern index
+            int textIterator = 0; // text index
+
+            while (textIterator < text.Length)
+            {
+                if (pattern[patternIterator] == text[textIterator])
+                {
+                    patternIterator++;
+                    textIterator++;
+                }
+                if (patternIterator == pattern.Length)
+                {
+                    Console.WriteLine("Znaleziono wzorzec na pozycji " + (textIterator - patternIterator));
+                    patternIterator = prefixTable[patternIterator - 1];
+                }
+
+                else if (textIterator < text.Length && pattern[patternIterator] != text[textIterator])
+                {
+
+                    if (patternIterator != 0)
+                        patternIterator = prefixTable[patternIterator - 1];
+                    else
+                        textIterator = textIterator + 1;
+                }
+            }
+
+
+        }
+
+        private int[] Prefix_Function(string pattern)
+        {
+            int len = 0;
+            int i = 1;
+            int[] prefixTable =  new int[pattern.Length];
+            prefixTable[0] = 0; 
+
+            while (i < pattern.Length)
+            {
+                if (pattern[i] == pattern[len])
+                {
+                    len++;
+                    prefixTable[i] = len;
+                    i++;
+                }
+                else
+                {
+        
+                    if (len != 0)
+                    {
+                        len = prefixTable[len - 1];
+
+                    }
+                    else 
+                    {
+                        prefixTable[i] = len;
+                        i++;
+                    }
+                }
+            }
+            return prefixTable;
+        }
+    }
+}
---
+++ PatternsInTextAlgorithms/PatternsInTextAlgorithms/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsInTextAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Algorithms algorithms  =  new Algorithms();
+
+            Console.WriteLine("\nTEST2");
+            string text = File.ReadAllText("tekst.txt").Replace("\n","");
+            string pattern = File.ReadAllText("wzorzec.txt").Replace("\n", "");
+
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            algorithms.Naive(pattern,text);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("\n<--Czas działana algorytmu naiwnego: "+elapsedMs + " ms-->\n");
+
+
+            watch.Reset();
+            watch.Start();
+            algorithms.Rabin_Karp(pattern,text,128,27077);
+            watch.Stop();
+            var rabinTime = watch.ElapsedMilliseconds;
+            Console.WriteLine("\n<--Czas działana algorytmu Rabina Karpa: " + rabinTime + " ms-->\n");
+
+
+            watch.Reset();
+            watch.Start();
+            algorithms.KMP(text, pattern);
+            watch.Stop();
+            var KMPTime = watch.ElapsedMilliseconds;
+            Console.WriteLine("\n<--Czas działana algorytmu KMP: " + KMPTime + " ms-->\n");
+
+
+
+            Console.WriteLine("\nTEST2");
+
+             text = File.ReadAllText("tekst2.txt").Replace("\n", "");
+             pattern = File.ReadAllText("wzorzec2.txt").Replace("\n", "");
+
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            algorithms.Naive(pattern, text);
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("\n<--Czas działana algorytmu naiwnego: " + elapsedMs + " ms-->\n");
+
+
+            watch.Reset();
+            watch.Start();
+            algorithms.Rabin_Karp(pattern, text, 128, 27077);
+            watch.Stop();
+            rabinTime = watch.ElapsedMilliseconds;
+            Console.WriteLine("\n<--Czas działana algorytmu Rabina Karpa: " + rabinTime + " ms-->\n");
+
+
+            watch.Reset();
+            watch.Start();
+            algorithms.KMP(text, pattern);
+            watch.Stop();
+            KMPTime = watch.ElapsedMilliseconds;
+            Console.WriteLine("\n<--Czas działana algorytmu KMP: " + KMPTime + " ms-->\n");
+
+
+            Console.ReadKey();
+        }
+
+
+        
+            
+    }
+}
+
